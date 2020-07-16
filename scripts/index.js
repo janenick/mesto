88e171bce5838f48b926d6d89400f58e5e89da23
Evgeniya Nikitina <janebornick@yandex.ru>
2020-07-16 23:03:46 +0300

    feat; add/del/like cards

--- scripts/index.js
+++ scripts/index.js
@@ -6,25 +6,24 @@ const addButton = document.querySelector(".profile__btn-add");
 const profileName = profileInfo.querySelector(".profile__name");
 const profileStatus = profileInfo.querySelector(".profile__status");
 
-const popupProfileInfo = document.querySelector(".popup_profile");
+const popupProfileInfo = document.querySelector(".popup_type_profile");
 const popupFormElement = popupProfileInfo.querySelector(".popup__container");
 const saveButton = popupProfileInfo.querySelector(".popup__btn-save");
 const closeButton = popupProfileInfo.querySelector(".popup__btn-close");
 const popupName = popupProfileInfo.querySelector(".popup__input_type_name");
 const popupStatus = popupProfileInfo.querySelector(".popup__input_type_status");
 
-const popupNewPlace = document.querySelector(".popup_new-place");
-const popupFormElementNewPlace = popupNewPlace.querySelector(
-  ".popup__container"
-);
+const popupNewPlace = document.querySelector(".popup_type_new-place");
+const popupFormElementNewPlace = popupNewPlace.querySelector(".popup__container");
 const saveButtonNewPlace = popupNewPlace.querySelector(".popup__btn-save");
 const closeButtonNewPlace = popupNewPlace.querySelector(".popup__btn-close");
-const popupNameNewPlace = popupNewPlace.querySelector(
-  ".popup__input_type_new-place-name"
-);
-const popupImgNewPlace = popupNewPlace.querySelector(
-  ".popup__input_type_new-place-img"
-);
+const popupNameNewPlace = popupNewPlace.querySelector(".popup__input_type_new-place-name");
+const popupImgNewPlace = popupNewPlace.querySelector(".popup__input_type_new-place-img");
+
+const popupBigImg = document.querySelector(".popup_type_img");
+const closeButtonBigImg = popupBigImg.querySelector(".popup__btn-close");
+const cardBigImg = popupBigImg.querySelector(".popup__img");
+const captionBigImg = popupBigImg.querySelector(".popup__caption");
 
 const elementsSection = document.querySelector(".elements");
 const newCardTemplate = document.querySelector("#element-template").content;
@@ -68,7 +67,6 @@ const initialCards = [
 function togglePopup(currentPopup) {
   /*const togglePopup = () => {*/
   currentPopup.classList.toggle("popup_opened");
-  
 }
 
 const editPopupProfile = () => {
@@ -89,7 +87,7 @@ const changeLike = (event) => {
   // в переменной eventTarget окажется элемент
   // button, на который мы кликнули
 
-    const eventTarget = event.target;
+  const eventTarget = event.target;
   eventTarget.classList.toggle("element__btn-like_active");
 };
 
@@ -101,34 +99,49 @@ const deleteCard = (event) => {
   cardItem.remove();
 };
 
+const showImage = (event) => {
+  console.log("Вы нажали на картинку");
+  togglePopup(popupBigImg);
+  const eventTarget = event.target;
+  const cardItem = eventTarget.closest(".element");
+
+  const cardImg = cardItem.querySelector(".element__img");
+
+  cardBigImg.src = cardImg.src;
+  captionBigImg.textContent = cardItem.querySelector(".element__title").textContent;
+};
+
 const addCard = (cardName, cardLink) => {
   const newCardElement = newCardTemplate.cloneNode(true);
-
   // наполняем содержимым
   newCardElement.querySelector(".element__title").textContent = cardName;
-  newCardElement.querySelector(".element__img").src = cardLink;
-  newCardElement.querySelector(".element__img").alt = cardName;
+  const cardImg = newCardElement.querySelector(".element__img");
+  cardImg.src = cardLink;
+  cardImg.alt = cardName;
+
+  cardImg.onclick = () => showImage(event);
 
   //поставим сердечку обработчик клика, при котором в консоль выводится объект evt:
   const newCardLikeButton = newCardElement.querySelector(".element__btn-like");
   newCardLikeButton.addEventListener("click", () => changeLike(event));
+
   // добавим "корзину"
   const newCardDelButton = newCardElement.querySelector(".element__btn-trash");
   newCardDelButton.addEventListener("click", () => deleteCard(event));
+
   // отображаем на странице
   elementsSection.prepend(newCardElement);
 };
 
-const openPopupNewplace = () => {
+const openPopupNewPlace = () => {
   togglePopup(popupNewPlace);
-  popupNameNewPlace.value = "Название";
-  popupImgNewPlace.value = "Ссылка на картинку";
 };
 
 const saveNewPlace = (event) => {
   event.preventDefault();
   addCard(popupNameNewPlace.value, popupImgNewPlace.value);
-
+  popupNameNewPlace.value = "Название";
+  popupImgNewPlace.value = "Ссылка на картинку";
   togglePopup(popupNewPlace);
 };
 
@@ -142,12 +155,13 @@ editButton.addEventListener("click", editPopupProfile);
 popupFormElement.addEventListener("submit", saveProfile);
 closeButton.addEventListener("click", () => togglePopup(popupProfileInfo));
 
-addButton.addEventListener("click", openPopupNewplace);
+addButton.addEventListener("click", openPopupNewPlace);
 popupFormElementNewPlace.addEventListener("submit", saveNewPlace);
 closeButtonNewPlace.addEventListener("click", () => togglePopup(popupNewPlace));
 
-//likeButton.addEventListener("click", () => changeLike);
-likeButton.addEventListener("click", changeLike);
-trashButton.addEventListener("click", deleteCard);
+//likeButton.addEventListener("click", changeLike);
+//trashButton.addEventListener("click", deleteCard);
+
+closeButtonBigImg.addEventListener("click", () => togglePopup(popupBigImg));
 
 addCards(initialCards);
